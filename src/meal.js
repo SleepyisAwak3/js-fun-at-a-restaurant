46c88cc5aa83df278c184ebf8b7b551b58b93385
Noah Byers <noahxbyers@gmail.com>
2023-02-01 16:46:23 -0800

    initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,40 @@
-function nameMenuItem() {
-
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`
 }
 
-
+function createMenuItem(menuItemName, price, type) {
+  var item = {
+    name: `${menuItemName}`,
+    price: `${price}`,
+    type: `${type}`
+  }
+  return item
+}
+function addIngredients(string,ingredients) {
+  if (ingredients.includes(string) !== true ) { 
+    ingredients.push(string) 
+    }  
+}
+function formatPrice(price,) {
+  return `$${price}`
+}
+function decreasePrice(price) {
+    return price * .9;
+  }
+function createRecipe(title, ingredients, type) {
+return {
+  title,
+  ingredients,
+  type,
+  }
+}
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
